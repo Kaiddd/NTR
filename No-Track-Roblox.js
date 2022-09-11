8d893c2a2415578d11f64b091dec1085d732a936
Kaiddd <37759450+Kaiddd@users.noreply.github.com>
2022-09-11 20:53:40 +0000

    Remove usage of /refer
    Before /refer was just getting it's un-needed queries nuked, but I thought about it and, why let Roblox use the /refer endpoint for games on the discover page in the first place? Now the discover page games just directly link to the games, boom!

--- No-Track-Roblox.js
+++ No-Track-Roblox.js
@@ -1,10 +1,16 @@
 // ==UserScript==
 // @name         No-Track Roblox (NTR)
-// @version      1.0
+// @version      1.1
 // @description  Removes Roblox's tracking from game redirects
 // @author       Kaid#0001
 // @match        *://*.roblox.com/*
 // ==/UserScript==
+
+/*
+Changelog:
+1.1: Removed /refer entirely!
+1.0: Initial Release
+*/
 
 // Only meant for stopping the tracking of game redirects, if you'd like an extension with general page ref id blocking, check this out: https://addons.mozilla.org/en-US/firefox/addon/clearurls/
 // May add more tracker-blocking in the future :Shrug:
@@ -24,7 +30,7 @@
                     let gameId = el.href.match(/PlaceId.+?(?=&)/g)[0]
                     if (gameId) {
                         gameId = gameId.match(/\d+/g)[0]
-                        el.href = el.href.replace(/(?=\?).*/g,"?PlaceId="+gameId)
+                        el.href = "https://roblox.com/games/"+gameId+"/"
                     }
                 }else{
                     el.href = el.href.replace(/(?=\?).*/g,"")
